ccd7adf08189388c6bd9beaba6b1500e93cb8786
fatemah-alm <fatemah.alm@hotmail.com>
2022-04-06 03:14:46 +0300

    winner winner chicken dinner

--- calculator.py
+++ calculator.py
@@ -1,7 +1,29 @@
 
 def main():
 	#write your code here
-	pass
+	try: 
+		x = int(input('Enter the first number: '))
+		y = int(input('Enter the second number: '))
+	except ValueError: 
+			print('invalid input, must be a number')
+			return
+				
+	operation = input('Choose the operation (+, -, /, *): ') 
+	match operation:
+			case '+': 
+				print(x + y)
+			case '-':
+				print(x - y)
+			case '/':
+				print(x/y)
+			case '*':
+				print(x*y)
+			case _: 
+				print('operation is not valid')
+
+
+
+	
 	
 
 
